aa288c9c972ffce6989ada049d68f94e52b1c825
Manasseh <chegemanasseh2@gmail.com>
2024-11-24 00:28:03 +0300

    Added the serialization

--- server/models.py
+++ server/models.py
@@ -11,21 +11,26 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model,SerializerMixin):
     __tablename__ = 'customers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    items = association_proxy('reviews', 'item')
+
     #relationships
     reviews = db.relationship('Review', back_populates='customer')
+
+    #Serialization rules 
+    serialize_rules = ('-reviews.customer',)
 
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model,SerializerMixin):
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -35,11 +40,14 @@ class Item(db.Model):
     #relationships
     reviews = db.relationship('Review', back_populates='item')
 
+    #Serialization rules
+    serialize_rules = ('-reviews.item',)
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
 
 
-class Review(db.Model):
+class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
 
     #columns
@@ -51,6 +59,9 @@ class Review(db.Model):
     #relationships
     customer = db.relationship('Customer', back_populates='reviews')
     item = db.relationship('Item', back_populates='reviews')
+    
+    #Serialization rules
+    serialize_rules = ('-customer.reviews', '-item.reviews')
 
     def __repr__(self):
         return f'<Review {self.id}, {self.comment}>'
